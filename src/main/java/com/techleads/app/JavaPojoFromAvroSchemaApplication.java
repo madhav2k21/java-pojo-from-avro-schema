ebc5a4111816c6782c4619aea3bbb27b4fd63557
madhav2k21 <madhav.anupoju.2k21@gmail.com>
2021-07-16 10:05:25 +0530

    Generates POJO classes from AVRO schema

---
+++ src/main/java/com/techleads/app/JavaPojoFromAvroSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.techleads.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPojoFromAvroSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPojoFromAvroSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techleads/app/avro/JavaPojoFromAvroSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techleads.app.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPojoFromAvroSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
